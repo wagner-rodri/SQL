15f62cc987c93ceda0e6046a590d60500d0b7b49
wagner-rodri <65721334+wagner-rodri@users.noreply.github.com>
2021-10-24 02:08:10 -0300

    Update CRUD_Exercise.sql
    Add coments

--- CRUD_Exercise.sql
+++ CRUD_Exercise.sql
@@ -1,3 +1,8 @@
+/*
+ * Exercício com a finalidade de praticar os comandos CRUD.
+ * Ele simula o CRUD em um guarda roupa
+ */
+
 CREATE DATABASE shirts_db;
 
 USE shirts_db;
@@ -9,7 +14,8 @@ CREATE TABLE shirts(
 	shirt_size VARCHAR(100),
 	last_worn INT
 	);
-	
+
+-- Adicione os dados à tabela criada
 INSERT INTO shirts (article, color, shirt_size, last_worn)
 VALUES 	('t-shirt', 'white', 'S', 10),
 		('t-shirt', 'green', 'S', 200),
@@ -19,12 +25,15 @@ VALUES 	('t-shirt', 'white', 'S', 10),
 		('polo shirt', 'red', 'M', 5),
 		('tank top', 'white', 'S', 200),
 		('tank top', 'blue', 'M', 15);
-		
+
+-- Adicione manualmente um novo item
 INSERT INTO shirts (article, color, shirt_size, last_worn)
 VALUES	('polo shirt', 'purple', 'M', 50);
 
+-- Selecione apenas as colunas article e color
 SELECT article, color FROM shirts;
 
+-- Selecione todas as colunas, menos a shirt_id, com roupas tamanho M
 SELECT 	article, 
 		color, 
 		shirt_size, 
@@ -32,21 +41,28 @@ SELECT 	article,
 FROM shirts 
 WHERE shirt_size = 'M';
 
+-- Atualize as roupas polo shirt, alterando seu tamanho para L
 UPDATE shirts SET shirt_size = 'L'
 WHERE article = 'polo shirt';
 
+-- Atualize o campo last_worn para 0 onde estiver 15
 UPDATE shirts SET last_worn = 0
 WHERE last_worn = 15;
 
+-- Atualize os campos color e shirt_size para XS e off white, respectivamente,  onde a cor for white
 UPDATE shirts SET shirt_size = 'XS', color = 'off white'
 WHERE color = 'white';
 
+-- Delete as llinhas onde last_worn for 200
 DELETE FROM shirts 
 WHERE last_worn = 200;
 
+-- Delete as linhas onde article for tank top
 DELETE FROM shirts
 WHERE article = 'tank top';
 
+-- Delete todas as linhas da tabela shirts
 DELETE FROM shirts;
 
+-- Delete a tabela shirts da base
 DROP TABLE shirts;
